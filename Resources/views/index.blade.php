7d5ff03f8112c7dc8a933ddb430f26ad5333d2fd
Uzzair Baharudin <uzzaircode@gmail.com>
2019-01-10 12:12:27 +0800

    initial commit of sparta module

---
+++ Resources/views/index.blade.php
@@ -0,0 +1,9 @@
+@extends('sparta::layouts.master')
+
+@section('content')
+    <h1>Hello World</h1>
+
+    <p>
+        This view is loaded from module: {!! config('sparta.name') !!}
+    </p>
+@stop
